d8af6ab7f941f460fad4519eed2797705b6afe61
Leonardo Albuquerque <leoplay2007@hotmail.com>
2024-06-18 13:50:05 -0300

    feat: create header

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,10 +1,32 @@
-import { Text, View } from 'react-native';
+import { Image, View, Text } from 'react-native';
 import { styles } from './styles';
+import { Feather } from '@expo/vector-icons';
+import { theme } from '../../global/styles/theme';
 
 export default function Dashboard() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}> Dashboard </Text>
+      <View style={styles.header}>
+        <View style={styles.userWrapper}>
+          <View style={styles.userInfo}>
+            <Image
+              style={styles.profileImage}
+              source={{
+                uri: 'https://avatars.githubusercontent.com/u/37962036?v=4',
+              }}
+            />
+            <View>
+              <Text style={styles.greeting}>Olá,</Text>
+              <Text style={styles.username}>Leonardo</Text>
+            </View>
+          </View>
+          <Feather
+            name='power'
+            size={24}
+            color={theme.colors.secondary}
+          />
+        </View>
+      </View>
     </View>
   );
 }
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,18 +1,51 @@
 import { StyleSheet } from 'react-native';
 import { theme } from '../../global/styles/theme';
+import { RFPercentage, RFValue } from 'react-native-responsive-fontsize'
 
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
     backgroundColor: theme.colors.background,
   },
-  title: {
-    color: theme.colors.title,
-    fontFamily: theme.fonts.bold,
-    fontSize: 20,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  }
+  header: {
+    height: RFPercentage(42),
+    backgroundColor: theme.colors.primary,
+    alignItems: 'center',
+    justifyContent: 'center',
+
+  },
+  userWrapper: {
+    width: '100%',
+    paddingHorizontal: 24,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  userInfo: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 16
+  },
+  profileImage: {
+    width: RFValue(48),
+    height: RFValue(48),
+    borderRadius: RFValue(10),
+    backgroundColor: theme.colors.shape,
+    borderWidth: 1,
+    borderColor: theme.colors.shape,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  greeting: {
+    color: theme.colors.shape,
+    fontSize: RFValue(18),
+    fontFamily: theme.fonts.regular,
+    lineHeight: RFValue(24),
+  },
+  username: {
+    color: theme.colors.shape,
+    lineHeight: RFValue(24),
+    fontSize: RFValue(18),
+    fontFamily: theme.fonts.bold
+  },
 });
