dd9d59bb320f30dcbdfb7f767cf25e6f3b146c69
Leonardo Albuquerque <leoplay2007@hotmail.com>
2024-06-18 07:13:21 -0300

    feat: add global styles

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,15 @@
+export default {
+  colors: {
+    primary: '#5636D3',
+    secondary: '#FF872C',
+    secondaryLight: 'rgba(255, 135, 44, .3)',
+    success: '#12A454',
+    successLight: 'rgba(18, 164, 84, .3)',
+    attention: '#E83F5b',
+    attentionLight: 'rgba(232, 63, 91, 0.5)',
+    shape: '#fff',
+    title: '#363F5F',
+    text: '#9C9Cb2',
+    background: '#F0F2F5',
+  }
+}
