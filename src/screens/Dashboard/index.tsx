7eb20418357bfac2875a33ce4b04262e6dc39212
Leonardo Albuquerque <leonardo@macrolabs.com.br>
2024-06-23 17:45:45 -0300

    feat: update cards and loading

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,4 +1,5 @@
 import { Feather } from '@expo/vector-icons';
+import { ActivityIndicator } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import { useCallback, useEffect, useState } from 'react';
@@ -15,20 +16,62 @@ import { theme } from '../../global/styles/theme';
 import { styles } from './styles';
 import { transactionMapper } from '../../mappers/transaction.mapper';
 import { useFocusEffect } from '@react-navigation/native';
+import { convertToBRLFormat } from '../../global/helpers/convert-to-brl-format.helper';
 
 export interface TransactionCardsList extends TransactionData {
   id: string;
 }
 
+export interface TransactionCardsData {
+  incomes: { amount: string };
+  expenses: { amount: string };
+  total: { amount: string };
+}
+
 export default function Dashboard() {
   const collectionKey: string = globalConfig.collectionKey;
-  const [data, setData] = useState<TransactionCardsList[]>([]);
+  const [transactions, setTransactions] = useState<TransactionCardsList[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [transactionSummary, setTransactionSummary] =
+    useState<TransactionCardsData | null>(null);
+  let entriesSum: number = 0;
+  let expenseSum: number = 0;
+
+  function calcExpenses(data: TransactionCardsList[]): number {
+    const expenseSum = data.reduce((accumulator, currentValue) => {
+      if (currentValue.transactionType === TransactionTypeEnum.outcome) {
+        return accumulator + Number(currentValue.amount);
+      } else {
+        return accumulator;
+      }
+    }, 0);
+    return expenseSum;
+  }
+
+  function calcEntries(data: TransactionCardsList[]): number {
+    const entriesSum = data.reduce((accumulator, currentValue) => {
+      if (currentValue.transactionType === TransactionTypeEnum.income) {
+        return accumulator + Number(currentValue.amount);
+      } else {
+        return accumulator;
+      }
+    }, 0);
+    return entriesSum;
+  }
 
   async function loadTransactions(): Promise<void> {
     const dataStorage = await AsyncStorage.getItem(collectionKey);
     const currentData = dataStorage ? JSON.parse(dataStorage) : [];
     const mappedData = currentData.map(transactionMapper);
-    setData(mappedData);
+    entriesSum = calcEntries(currentData);
+    expenseSum = calcExpenses(currentData);
+    setTransactions(mappedData);
+    setTransactionSummary({
+      incomes: { amount: convertToBRLFormat(entriesSum) },
+      expenses: { amount: convertToBRLFormat(expenseSum) },
+      total: { amount: convertToBRLFormat(entriesSum - expenseSum) },
+    });
+    setIsLoading(false);
   }
 
   useEffect(() => {
@@ -43,64 +86,78 @@ export default function Dashboard() {
 
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <View style={styles.userWrapper}>
-          <View style={styles.userInfo}>
-            <Image
-              style={styles.profileImage}
-              source={{
-                uri: 'https://avatars.githubusercontent.com/u/37962036?v=4',
-              }}
-            />
-            <View>
-              <Text style={styles.greeting}>Olá,</Text>
-              <Text style={styles.username}>Leonardo</Text>
+      {isLoading ? (
+        <ActivityIndicator
+          style={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+          color={theme.colors.primary}
+          size='large'
+        />
+      ) : (
+        <>
+          <View style={styles.header}>
+            <View style={styles.userWrapper}>
+              <View style={styles.userInfo}>
+                <Image
+                  style={styles.profileImage}
+                  source={{
+                    uri: 'https://avatars.githubusercontent.com/u/37962036?v=4',
+                  }}
+                />
+                <View>
+                  <Text style={styles.greeting}>Olá,</Text>
+                  <Text style={styles.username}>Leonardo</Text>
+                </View>
+              </View>
+              <BorderlessButton onPress={() => {}}>
+                <Feather
+                  name='power'
+                  size={24}
+                  color={theme.colors.secondary}
+                />
+              </BorderlessButton>
             </View>
           </View>
-          <BorderlessButton onPress={() => {}}>
-            <Feather
-              name='power'
-              size={24}
-              color={theme.colors.secondary}
+          <ScrollView
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            style={styles.transactionCardsWrapper}
+            contentContainerStyle={styles.transactionCards}
+          >
+            <TransactionCard
+              title='Entrada'
+              amount={transactionSummary?.incomes.amount ?? '0'}
+              lastTransaction='Última entrada dia 13 de abril'
+              type={TransactionTypeEnum.income}
             />
-          </BorderlessButton>
-        </View>
-      </View>
-      <ScrollView
-        horizontal
-        showsHorizontalScrollIndicator={false}
-        style={styles.transactionCardsWrapper}
-        contentContainerStyle={styles.transactionCards}
-      >
-        <TransactionCard
-          title='Entrada'
-          amount='R$ 17.400,00'
-          lastTransaction='Última entrada dia 13 de abril'
-          type={TransactionTypeEnum.income}
-        />
-        <TransactionCard
-          title='Saídas'
-          amount='R$ 1.259,00'
-          lastTransaction='Última saída dia 03 de abril'
-          type={TransactionTypeEnum.outcome}
-        />
-        <TransactionCard
-          title='Total'
-          amount='R$ 16.141,00'
-          lastTransaction='01 à 16 de abril'
-          type={TransactionTypeEnum.total}
-        />
-      </ScrollView>
-      <View style={styles.transactions}>
-        <Text style={styles.transactionsTitle}>Listagem</Text>
-        <FlatList
-          data={data}
-          keyExtractor={(item) => String(item.id)}
-          renderItem={({ item }) => <TransactionItem data={item} />}
-          showsVerticalScrollIndicator={false}
-          contentContainerStyle={styles.transactionsList}
-        />
-      </View>
+            <TransactionCard
+              title='Saídas'
+              amount={transactionSummary?.expenses.amount ?? '0'}
+              lastTransaction='Última saída dia 03 de abril'
+              type={TransactionTypeEnum.outcome}
+            />
+            <TransactionCard
+              title='Total'
+              amount={transactionSummary?.total.amount ?? '0'}
+              lastTransaction='01 à 16 de abril'
+              type={TransactionTypeEnum.total}
+            />
+          </ScrollView>
+          <View style={styles.transactions}>
+            <Text style={styles.transactionsTitle}>Listagem</Text>
+            <FlatList
+              data={transactions}
+              keyExtractor={(item) => String(item.id)}
+              renderItem={({ item }) => <TransactionItem data={item} />}
+              showsVerticalScrollIndicator={false}
+              contentContainerStyle={styles.transactionsList}
+            />
+          </View>
+        </>
+      )}
     </View>
   );
 }
